80973da9d33d2bd4d9184a01c91876703a023e4b
zenshir0 <pave1kagg@gmail.com>
2025-03-18 22:18:43 +0300

    home section improvements

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { img1: 'res/keyboard2.webp', img2: 'res/keyboard2.1.webp', title: 'Sakura R2 108', desc: 'Механика; Hotswap; Kailh Silent', price: 'от 17300 ₽' },
         {
             img1: 'https://cdn.shopify.com/s/files/1/0638/1420/5659/files/109.png?v=1711520208',
-            img2: 'https://cdn.shopify.com/s/files/1/0638/1420/5659/files/109_81e037c1-c711-48f2-8d0f-4429886b7f7b.png?v=1711520238',
+            img2: 'https://varmilo.com/cdn/shop/files/moonlight.jpg?v=1712042704&width=493',
             title: 'Moonlight 88/109',
             desc: 'Механика; No Hotswap; EC Iris, EC Sakura, EC Rose',
             price: 'от 9078 ₽'
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             img1: 'https://cdn.shopify.com/s/files/1/0638/1420/5659/files/3_89214f38-9675-447a-bd18-82998b5fa90a.jpg?v=1706518055',
             img2: 'https://cdn.shopify.com/s/files/1/0638/1420/5659/files/4_33c437b7-7b6e-4a8e-8f73-aae5f3f37786.jpg?v=1706522249',
-            title: 'Aurora 87108109',
+            title: 'Aurora',
             desc: 'Механика; Hotswap; Kailh Prestige Silent',
             price: 'от 15810 ₽'
         }
@@ -140,7 +140,6 @@ document.addEventListener('DOMContentLoaded', () => {
         toggleDisabledForButton(emailRegister, passwordRegister, registerBtn, passwordConfirmRegister);
     });
 
-    
 });
 
 function toggleDisabledForButton(emailInput, passwordInput, buttonToToggle, passwordConfirmInput = null) {
